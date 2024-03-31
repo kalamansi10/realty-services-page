dbf62cf7fa87e0700c0eaac2218d7ea872d95d01
kalamansi10 <123523157+kalamansi10@users.noreply.github.com>
2024-03-31 23:36:54 +0800

    Scroll animations for article contents

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-const navBar = document.querySelector(".nav-bar")
+const navBar = document.querySelector(".nav");
+
 const introSection = document.getElementById("service-intro-section");
+
 const servicesContainer = document.querySelector(".services-container");
 const servicesCards = document.querySelectorAll(".services-card");
+
+const articleLines = document.querySelectorAll(".article-line");
+
+const articleImages2 = document.querySelectorAll(".article-img");
+
+const sellingProcessContainer = document.querySelector(".selling-process");
+const buyingProcessContainer = document.querySelector(".buying-process");
+const processesListLeft = document.querySelectorAll(".list-left");
+const processesListRight = document.querySelectorAll(".list-right");
+
+const contactSection = document.querySelector(".contact-content");
 
 const introObserver = new IntersectionObserver(
   (entries) => {
     if (entries[0].isIntersecting) {
       navBar.classList.remove("nav-bar-others");
       navBar.classList.add("nav-bar-intro");
-      document.addEventListener("scroll", removeIntro);
     } else {
       navBar.classList.remove("nav-bar-intro");
       navBar.classList.add("nav-bar-others");
-      document.removeEventListener("scroll", removeIntro);
     }
   },
   {
-    threshold: 0,
+    threshold: 0.1,
   }
 );
 
@@ -35,15 +46,74 @@ const introMessageObserver = new IntersectionObserver(
 
 const servicesObserver = new IntersectionObserver(
   (entries) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add("up-fade-in");
+      servicesObserver.unobserve(entries[0].target)
+      servicesCards.forEach((card, i) => {
+        setTimeout(() => card.classList.add("right-fade-in"), (i+1) * 500);
+      });
+    }
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+const sellingProcessObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add("zoom-in");
+      sellingProcessObserver.unobserve(entries[0].target)
+      processesListLeft.forEach((card, i) => {
+        setTimeout(() => card.classList.add("right-fade-in"), (i+1) * 400);
+      });
+    }
+  },
+  {
+    threshold: 0,
+  }
+);
+
+const buyingProcessObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add("zoom-in");
+      buyingProcessObserver.unobserve(entries[0].target)
+      processesListRight.forEach((card, i) => {
+        setTimeout(() => card.classList.add("right-fade-in"), (i+1) * 500);
+      });
+    }
+  },
+  {
+    threshold: 0,
+  }
+);
+
+const lineObserver = new IntersectionObserver(
+  (entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("zoom-in")
-        servicesObserver.unobserve(entry.target);
+        entry.target.classList.add("line-animation");
+        lineObserver.unobserve(entry.target);
       }
     })
   },
   {
-    threshold: .6,
+    threshold: 1,
+  }
+);
+
+const imageObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("up-fade-in");
+        imageObserver.unobserve(entry.target);
+      }
+    })
+  },
+  {
+    threshold: 0,
   }
 );
 
@@ -61,16 +131,42 @@ function smoothScroll() {
   const element = document.getElementById("services-section");
   element.scrollIntoView({
     behavior: "smooth",
-    block: "end",
+    block: "start",
     inline: "nearest",
   });
   navBar.classList.remove("nav-bar-intro");
   navBar.classList.add("nav-bar-others");
 }
 
+const contactObserver = new IntersectionObserver(
+  (entries) => {
+    if (entries[0].isIntersecting) {
+      entries[0].target.classList.add("zoom-in");
+      contactObserver.unobserve(entries[0].target);
+    }
+  },
+  {
+    threshold: 0,
+  }
+);
+
+document.addEventListener("scroll", removeIntro);
+
 introObserver.observe(introSection);
+
 introMessageObserver.observe(introSection);
-servicesCards.forEach(card => {
-  servicesObserver.observe(card);
+
+servicesObserver.observe(servicesContainer);
+
+articleLines.forEach(line => {
+  lineObserver.observe(line);
 })
 
+articleImages2.forEach(image => {
+  imageObserver.observe(image);
+})
+
+sellingProcessObserver.observe(sellingProcessContainer);
+buyingProcessObserver.observe(buyingProcessContainer);
+
+contactObserver.observe(contactSection);
